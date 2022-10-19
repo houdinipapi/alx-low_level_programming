577df252478d4b976383b20cfbc64c5b0df1ee9e
houdinipapi <ochiengcliff.co@gmail.com>
2022-10-19 09:59:21 +0000

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
-*main - check the code
 *_abs - function that computes the absolute value
 *of an integer
+*@r: int used for function's argument
+*
 *Return: 0
 */
-int main(void)
+int _abs(int r)
 {
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-	return (0);
+	if (r < 0)
+		r = (-1) * r;
+	return (r);
 }
