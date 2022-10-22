40cdf5ba5f8f07046632d56b4818ba6c2edb8b7e
houdinipapi <ochiengcliff.co@gmail.com>
2022-10-22 14:44:36 +0000

    character char

---
+++ Jenny_lectures/1-jenny_char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*Description: this function prints a character and its ASCII value
+*Return
+*/
+
+void main(void)
+{
+	char a;
+
+	a = 'a';
+	printf("%c\n", a);
+	printf("%d\n", a);
+}
---
+++ Jenny_lectures/2-jenny_char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*Description: prints the character and its ASCII value
+*Return: success
+*/
+
+void main(void)
+{
+	char b;
+
+	b = 98;
+	printf("%c\n", b);
+	printf("%d\n", b);
+}
