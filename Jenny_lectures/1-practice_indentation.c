d9d1d90d82e415551cc891b37d0953a73610a0bc
houdinipapi <ochiengcliff.co@gmail.com>
2022-10-25 15:18:47 +0000

    practice makes perfect

---
+++ Jenny_lectures/1-practice_indentation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *Return: sum of x and y
+  */
+
+int main(void)
+{
+	int x = 3;
+	int y = 6;
+	int i = x + y;
+
+	printf("%d\n", i);
+
+	return (0);
+}
