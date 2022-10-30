d215b14eff2fa6bc67cddd076ea5b00cd203cf92
houdinipapi <ochiengcliff.co@gmail.com>
2022-10-30 13:06:04 +0000

    Sams teaching

---
+++ Sams-teach_yourself/02L01.c
@@ -0,0 +1,13 @@
+/* My first progam */
+#include <stdio.h>
+
+/**
+  *main - main function
+  *Return: success
+  */
+
+int main(void)
+{
+	printf("Houdini, you-guy! This is my first C program.\n");
+	return (0);
+}
