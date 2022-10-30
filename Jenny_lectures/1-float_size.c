877af682f658a9178c5f49e35d4f2d83550fb21d
houdinipapi <ochiengcliff.co@gmail.com>
2022-10-30 07:12:43 +0000

    float size

---
+++ Jenny_lectures/1-float_size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - main function
+  *Return: success
+  */
+
+int main(void)
+{
+	float a;
+
+	printf("Size of float in a 64-bit machine is %zu bytes\n", sizeof(a));
+}
