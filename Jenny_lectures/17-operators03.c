37183fbf3497179a166c364724fcd5194d646d43
houdinipapi <ochiengcliff.co@gmail.com>
2022-11-09 10:52:35 +0000

    Practical

---
+++ Jenny_lectures/17-operators03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 4, b = 6, result;
+
+	result = a > b && printf("Jenny\n") || printf("lectures\n") || printf("jk\n");
+	printf("%d\n", result);
+}
---
+++ Jenny_lectures/17-operators05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 1, b = 6, result;
+
+	result = --a && ++b;
+	printf("%d\n", result);
+}
