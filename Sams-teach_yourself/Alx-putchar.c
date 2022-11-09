1f61f9eef992e57daba06478134537e7566ab0ff
houdinipapi <ochiengcliff.co@gmail.com>
2022-11-09 15:11:42 +0000

    Practice

---
+++ Sams-teach_yourself/Alx-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - print _putchar, followed by a new line
+  *
+  *Description: the program should print _putchar
+  *followed by a new line
+  *Return: 0
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
