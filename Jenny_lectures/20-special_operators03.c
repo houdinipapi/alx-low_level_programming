90f29268916a629a69c6a9232be4ae82760a6acd
houdinipapi <ochiengcliff.co@gmail.com>
2022-11-16 09:44:14 +0000

    Comma operators

---
+++ Jenny_lectures/20-special_operators03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	a = (printf("Houdini\n"), 7, 3, 9);
+	printf("The value of a is: %d\n", a);
+
+	return (0);
+}
---
+++ Jenny_lectures/20-special_operators07.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 9, b;
+
+	b = a++, ++a, a >> 2;
+	printf("The value of a is: %d\n", a);
+	printf("The value of b is: %d\n", b);
+
+	return (0);
+}
