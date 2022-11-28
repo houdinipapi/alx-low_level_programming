ff22ec89ee87010e1cdcd61ed6aa04f19ffe9a5f
houdinipapi <ochiengcliff.co@gmail.com>
2022-11-28 12:50:26 +0000

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-void myStartuFun(void)__attribute__ ((constructor));
+void first(void)__attribute__ ((constructor));
 
 /**
-  * myStartupFun - Function that runs before main function
+  * first - Function that runs before main function
   */
 
-void myStartupFun(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore my house upon my back!\n");
